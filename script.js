7fe3540285c59b0b03369a14b1b140c6451f1c5e
paulaa99 <paau.a@hotmail.com>
2023-08-28 19:43:54 -0300

    script.js

--- script.js
+++ script.js
@@ -6,16 +6,35 @@ const PRODUCTS = [
 
 //Almacenar el ID de un producto en el localStorage y luego redireccionar al usuario a la página de detalles del producto.
 
+
+
 document.addEventListener("DOMContentLoaded", function () {
     const productList = document.getElementById("productList");
 
 // Utiliza un bucle para iterar a través de los productos.
+
+for (const product of PRODUCTS) {
+
 // Por cada producto, crea un elemento <li> y un enlace <a>.
+
+ const item = document.createElement("li");
+ const link = document.createElement("a");
+
 // Configura el texto del enlace con el nombre del producto.
+
+link.textContent = product.name;
 
 // Agrega un evento de clic al enlace. Cuando se haga clic en un producto,
 // deberás almacenar el ID del producto en el localStorage y redireccionar a "product-info.html".
 
-// Agrega el enlace al elemento <li> y agrega el <li> a la lista de productos.
+link.addEventListener("click", function () {
+    localStorage.setItem("selectedProductID", product.id);
+    window.location.href = "product-info.html";
+});
 
+// Agrega el enlace al elemento <li> y agrega el <li> a la lista de productos.
+  item.appendChild(link);
+  productList.appendChild(item)
+
+ }
 });
